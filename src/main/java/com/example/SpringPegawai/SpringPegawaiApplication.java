9aed64e206b441a95787bd1936041c2ae41bcf4e
rifkihaiqal <maulana.haiqal@myinfosys.net>
2020-07-27 10:52:14 +0700

    Tambah @EnableJpaAuditing

--- src/main/java/com/example/SpringPegawai/SpringPegawaiApplication.java
+++ src/main/java/com/example/SpringPegawai/SpringPegawaiApplication.java
@@ -2,8 +2,10 @@ package com.example.SpringPegawai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringPegawaiApplication {
 
 	public static void main(String[] args) {
