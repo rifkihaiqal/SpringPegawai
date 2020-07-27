4c464571f10d2e7cb9742d83ed0ea744180f6939
rifkihaiqal <maulana.haiqal@myinfosys.net>
2020-07-27 09:20:58 +0700

    First Commit

---
+++ src/main/java/com/example/SpringPegawai/SpringPegawaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringPegawai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPegawaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPegawaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringPegawai/SpringPegawaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringPegawai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPegawaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
